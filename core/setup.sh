98b39d4aac67ea243d5726c54a13c0864cd6b85e
Martin Levesque <levesque.martin@gmail.com>
2020-01-27 20:13:13 -0500

    wip

--- core/setup.sh
+++ core/setup.sh
@@ -1,4 +1,8 @@
 
+###########################################################
+### GENERAL
+set -e
+
 echo "Update ubuntu..."
 apt-get update
 
@@ -21,4 +25,31 @@ systemctl restart systemd-timesyncd.service
 timedatectl
 
 echo "installing fail2ban"
-apt install -y fail2ban # TODO add configurations
+apt install -y fail2ban
+
+cp configs/jail.conf /etc/fail2ban/jail.conf
+/etc/init.d/fail2ban restart
+/etc/init.d/fail2ban status
+
+echo "installing ufw"
+apt install -y ufw
+
+ufw allow 80
+ufw allow 443
+ufw allow 22
+ufw allow ntp
+
+echo "activating ufw"
+yes | ufw enable
+
+###########################################################
+# Internal services
+
+echo "installing redis"
+apt install -y redis-server
+
+echo "installing mysql server"
+apt install -y mysql-server
+mysql -h127.0.0.1 -uroot -e"USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES; "
+systemctl restart mysql.service
+mysql_secure_installation
