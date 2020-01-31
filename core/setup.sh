a4c4a3f3d91f1dbf5aa1577e2e21ca830c0f6498
Martin Levesque <levesque.martin@gmail.com>
2020-01-30 21:24:22 -0500

    update

--- core/setup.sh
+++ core/setup.sh
@@ -4,43 +4,43 @@
 set -e
 
 echo "Update ubuntu..."
-apt-get update
+sudo apt-get update
 
 echo "Upgrading ubuntu..."
-apt-get upgrade -y
+sudo apt-get upgrade -y
 
 echo "apt autoremove..."
-apt autoremove -y
+sudo apt autoremove -y
 
 echo "Upgrading ubuntu again..."
-apt-get upgrade -y
+sudo apt-get upgrade -y
 
 echo "apt autoremove again..."
-apt autoremove -y
+sudo apt autoremove -y
 
 echo "installing ntp"
-apt install -y ntp
-timedatectl set-ntp true
-systemctl restart systemd-timesyncd.service
-timedatectl
+sudo apt install -y ntp
+sudo timedatectl set-ntp true
+sudo systemctl restart systemd-timesyncd.service
+sudo timedatectl
 
 echo "installing fail2ban"
-apt install -y fail2ban
+sudo apt install -y fail2ban
 
-cp configs/jail.conf /etc/fail2ban/jail.conf
-/etc/init.d/fail2ban restart
-/etc/init.d/fail2ban status
+sudo cp configs/jail.conf /etc/fail2ban/jail.conf
+sudo /etc/init.d/fail2ban restart
+sudo /etc/init.d/fail2ban status
 
 echo "installing ufw"
-apt install -y ufw
+sudo apt install -y ufw
 
-ufw allow 80
-ufw allow 443
-ufw allow 22
-ufw allow ntp
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 22
+sudo ufw allow ntp
 
 echo "activating ufw"
-yes | ufw enable
+yes | sudo ufw enable
 
 echo "Node.js"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
@@ -56,26 +56,26 @@ pm2 list
 
 ### Redis
 echo "installing redis"
-apt install -y redis-server
+sudo apt install -y redis-server
 
 ### Mysql
 echo "installing mysql server"
-apt install -y mysql-server
+sudo apt install -y mysql-server
 
 echo "----------------- EXECUTE IN MYSQL"
 echo "USE mysql; UPDATE user SET plugin='mysql_native_password' WHERE User='root'; FLUSH PRIVILEGES; "
-mysql -u root
+sudo mysql -u root
 
-systemctl restart mysql.service
-mysql_secure_installation
+sudo systemctl restart mysql.service
+sudo mysql_secure_installation
 
 echo "Import the database, make sure to have /var/www/openode.sql"
 read -p "Press enter to continue"
-mysql -u root -p  < /var/www/openode.sql
+sudo mysql -u root -p  < /var/www/openode.sql
 
 ### NGINX
 apt install -y nginx
-cp configs/nginx.conf /etc/nginx/nginx.conf
+sudo cp configs/nginx.conf /etc/nginx/nginx.conf
 
 echo "** Copy core certs openode.io.crt privatekey.key"
 read -p "Press enter to continue"
@@ -83,10 +83,10 @@ read -p "Press enter to continue"
 ### rvm, ruby
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
+source /home/ubuntu/.rvm/scripts/rvm
 rvm install 2.7.0
 echo '' >> ~/.bashrc
-echo 'source /etc/profile.d/rvm.sh' >> ~/.bashrc
+echo 'source /home/ubuntu/.rvm/scripts/rvm' >> ~/.bashrc
 
 ###########################################################
 # openode-www
