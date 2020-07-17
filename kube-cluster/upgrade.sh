e33b6edd92da47930192f3556724367ae74d90a2
Martin Lévesque <levesque.martin@gmail.com>
2020-07-16 21:07:08 -0400

    Update upgrade.sh

--- kube-cluster/upgrade.sh
+++ kube-cluster/upgrade.sh
@@ -1,2 +1,6 @@
 
-KUBECONFIG=development-canada.yml helm upgrade --reuse-values quickstart stable/nginx-ingress
+KUBECONFIG=development-canada.yml helm repo update
+
+KUBECONFIG=development-canada.yml helm delete quickstart
+
+KUBECONFIG=development-canada.yml helm install quickstart nginx-stable/nginx-ingress
