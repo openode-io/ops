27883df9a1e349806d05c482232109911fc400a0
Martin Levesque <levesque.martin@gmail.com>
2020-08-18 16:05:04 -0400

    proxy protocol

--- kube-cluster/setup.sh
+++ kube-cluster/setup.sh
@@ -6,3 +6,15 @@ KUBECONFIG=development-canada.yml helm install quickstart nginx-stable/nginx-ing
 # to get list of services, column 4 gives cname (subdomains)
 # quickstart-nginx-ingress-controller LoadBalancer 10.3.192.240 6ojq4g6ss0.lb.c1.bhs5.k8s.ovh.net   80:31114/TCP,443:30631/TCP   3m42s
 KUBECONFIG=development-canada.yml kubectl get services
+
+# PROXY PROTOCOL
+
+# Configure the NGINX configmap for proxy-protocol
+# kubectl edit configmap quickstart-nginx-ingress
+#data:
+#  proxy-protocol: "True"
+#  real-ip-header: proxy_protocol
+#  set-real-ip-from: 10.136.75.230 # <--- this is the IP obtained when not using this option
+
+# # and enable the proxy protocol
+# kubectl annotate service quickstart-nginx-ingress service.beta.kubernetes.io/do-loadbalancer-enable-proxy-protocol=true
--- kube-cluster/upgrade.sh
+++ kube-cluster/upgrade.sh
@@ -4,3 +4,5 @@ KUBECONFIG=development-canada.yml helm repo update
 KUBECONFIG=development-canada.yml helm delete quickstart
 
 KUBECONFIG=development-canada.yml helm install quickstart nginx-stable/nginx-ingress
+
+# make sure to check proxy protocol, option set-real-ip-from, see setup.sh
