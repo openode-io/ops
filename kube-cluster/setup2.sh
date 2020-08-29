2c97ca04987af6e275e47d422bddb961b328cd89
Martin Levesque <levesque.martin@gmail.com>
2020-08-29 11:48:28 -0400

    recreate strategy

--- kube-cluster/setup2.sh
+++ kube-cluster/setup2.sh
@@ -11,6 +11,9 @@ KUBECONFIG=development-test.yml kubectl edit deployment nginx-ingress-controller
 #  requests:
 #    memory: 500Mi
 
+# strategy:
+#   type: Recreate
+
 # add configmaps, with name corresponding to the one in deployment nginx-ingress-controller
 KUBECONFIG=development-test.yml kubectl apply -f ../../../ops/kube-cluster/nginx-configmaps.yml
 KUBECONFIG=development-test.yml kubectl annotate service nginx-ingress-controller service.beta.kubernetes.io/do-loadbalancer-enable-proxy-protocol=true
