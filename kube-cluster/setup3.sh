975f4f44651f90f27a8136ab92c995ffa959e353
Martin Levesque <levesque.martin@gmail.com>
2020-12-28 10:57:51 -0500

    add metrics-server

--- kube-cluster/setup3.sh
+++ kube-cluster/setup3.sh
@@ -16,4 +16,8 @@ KUBECONFIG=development-test.yml kubectl edit deployment nginx-ingress-ingress-ng
 
 # add configmaps, with name corresponding to the one in deployment nginx-ingress-controller
 KUBECONFIG=development-test.yml kubectl apply -f ../../../ops/kube-cluster/nginx-configmaps.yml
+
+# metrics server
+KUBECONFIG=development-test.yml kubectl apply -f ../../../ops/kube-cluster/metrics-server-components.yaml
+
 KUBECONFIG=development-test.yml kubectl annotate service nginx-ingress-ingress-nginx-controller service.beta.kubernetes.io/do-loadbalancer-enable-proxy-protocol=true
